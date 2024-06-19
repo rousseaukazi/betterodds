59153e1962d0fae542662cb0b7fb726d7418de06
rousseaukazi <kazi.rousseau@gmail.com>
2024-06-19 17:28:29 -0400

    Update app.py

--- app.py
+++ app.py
@@ -15,19 +15,19 @@ client = openai.OpenAI()
 # Streamlit title
 "# 021"
 
-st.sidebar.title("Navigation")
-page = st.sidebar.radio("Go to", ["Page 1", "Page 2"])
+# st.sidebar.title("Navigation")
+# page = st.sidebar.radio("Go to", ["Page 1", "Page 2"])
 
-if page == "Page 1":
-    import page1
-    page1.show()
-elif page == "Page 2":
-    import page2
-    page2.show()
+# if page == "Page 1":
+#     import page1
+#     page1.show()
+# elif page == "Page 2":
+#     import page2
+#     page2.show()
 
-# Capturing the idea
-# idea = st.text_input("What's your idea?")
-# suno_api_key = '72+hIHnhRYYVuu3v3CGc8P+QvZqSZJpk'
+Capturing the idea
+idea = st.text_input("What's your idea?")
+suno_api_key = '72+hIHnhRYYVuu3v3CGc8P+QvZqSZJpk'
 
 def check_remaining_quota(api_key):
     url = "https://api.sunoaiapi.com/api/v1/gateway/limit"
@@ -176,72 +176,74 @@ def ImageGen(prompt):
     )
     image_url = response.data[0].url
     return image_url
-# if idea: 
-#     # Prompts 
-#     ol_prompt = "I'm starting a company. This is my idea " + idea + ". Please provide me with 3 different one-liners I can use in my seed deck. Just provide me with the one-liners and nothing else."
-#     domain_prompt = "I'm starting a company. This is my idea " + idea + ". Please provide me with 3 different domains I can use in my seed deck. Just provide the domain name, a reason why, and nothing else."
-#     ms_prompt = "I'm starting a company. This is my idea " + idea + ". Please provide me with detailed yet concise bullet points detailing the market sizing. Use real numbers and figures. Just provide me with the market sizing and nothing else."
-#     income_prompt = "I'm starting a company. This is my idea " + idea + ". Please provide me with a few bullet points detailing the conservative to aggressive income projections based on the business model and various timelines. Just provide the income projections, reasoning behind it, and the unit economics and nothing else."
-#     proto_prompt = "I'm starting a company. My idea: " + idea + ". Provide a code block for the HTML and CSS for a modern looking website (with a beautiful gradient header) that has a clear marketing header, get started button, footer, and black border around the entire site so it's visible on a whitebackground. Your response should start and end with '''. Nothing else but just the code block. If you provide anything, it will break my product."
-#     logo_prompt = "I'm starting a company. This is my idea " + idea + ". Generate a simple, black icon for it similar to the style of the iconic apple or nike logo."
-#     song_prompt = "Create 4 stanzas of song lyrics for a pop, marketing song about my business idea. Here's the idea: " + idea + ". Just provide the lyrics no extranerous or confirmation text. It should start with (Verse 1)."
-#     song_title_prompt = "Create a title for a poppy, marketing song for my business idea. Here's the idea: " + idea + ". Just provide the title, nothing else."
 
-#     ## Refresh 
+if idea: 
+    # Prompts 
+    ol_prompt = "I'm starting a company. This is my idea " + idea + ". Please provide me with 3 different one-liners I can use in my seed deck. Just provide me with the one-liners and nothing else."
+    domain_prompt = "I'm starting a company. This is my idea " + idea + ". Please provide me with 3 different domains I can use in my seed deck. Just provide the domain name, a reason why, and nothing else."
+    ms_prompt = "I'm starting a company. This is my idea " + idea + ". Please provide me with detailed yet concise bullet points detailing the market sizing. Use real numbers and figures. Just provide me with the market sizing and nothing else."
+    income_prompt = "I'm starting a company. This is my idea " + idea + ". Please provide me with a few bullet points detailing the conservative to aggressive income projections based on the business model and various timelines. Just provide the income projections, reasoning behind it, and the unit economics and nothing else."
+    proto_prompt = "I'm starting a company. My idea: " + idea + ". Provide a code block for the HTML and CSS for a modern looking website (with a beautiful gradient header) that has a clear marketing header, get started button, footer, and black border around the entire site so it's visible on a whitebackground. Your response should start and end with '''. Nothing else but just the code block. If you provide anything, it will break my product."
+    logo_prompt = "I'm starting a company. This is my idea " + idea + ". Generate a simple, black icon for it similar to the style of the iconic apple or nike logo."
+    song_prompt = "Create 4 stanzas of song lyrics for a pop, marketing song about my business idea. Here's the idea: " + idea + ". Just provide the lyrics no extranerous or confirmation text. It should start with (Verse 1)."
+    song_title_prompt = "Create a title for a poppy, marketing song for my business idea. Here's the idea: " + idea + ". Just provide the title, nothing else."
+
+    ## Refresh 
+
+    title = ChatGPTNoStream(song_title_prompt).choices[0].message.content
+    lyrics = ChatGPTNoStream(song_prompt).choices[0].message.content
+
+    # Generate Song
     
-#     "## One Liner"
-#     prompts = get_prompts(idea)
-#     ChatGPT(prompts["OneLiner"])
-#     txt = st.text_area("OneLiner Prompt", prompts["OneLiner"])
-
-#     if st.button("Log"):
-#         st.write(len(txt))
-
-#     "## Domains"
-#     ChatGPT(domain_prompt)
-
-#     # "## Market Sizing"
-#     # # ChatGPT(ms_prompt)
-
-#     # "## Income Projections"
-#     # # ChatGPT(income_prompt)
-
-#     # "## Logo"
-#     # # image_url = str(ImageGen(logo_prompt))
-#     # # st.image(image_url)
+    music_generation_info = generate_music(suno_api_key, title, lyrics)
+    song_id = music_generation_info[0]["song_id"]
+    song_id = [song_id]
     
-#     "## Prototype"
-#     st.html(ChatGPTCode(proto_prompt))
+    "## One Liner"
+    prompts = get_prompts(idea)
+    ChatGPT(prompts["OneLiner"])
+    txt = st.text_area("OneLiner Prompt", prompts["OneLiner"])
 
-#     # "## Surprise"
-#     # title = ChatGPTNoStream(song_title_prompt).choices[0].message.content
-#     # lyrics = ChatGPTNoStream(song_prompt).choices[0].message.content
+    if st.button("Log"):
+        st.write(len(txt))
 
-#     # # Generate Song
+    "## Domains"
+    ChatGPT(domain_prompt)
 
-#     # latest_iteration = st.empty()
-#     # bar = st.progress(0)
+    "## Market Sizing"
+    ChatGPT(ms_prompt)
+
+    "## Income Projections"
+    ChatGPT(income_prompt)
+
+    "## Logo"
+    # image_url = str(ImageGen(logo_prompt))
+    # st.image(image_url)
     
-#     # music_generation_info = generate_music(suno_api_key, title, lyrics)
-#     # song_id = music_generation_info[0]["song_id"]
-#     # song_id = [song_id]
+    "## Prototype"
+    st.html(ChatGPTCode(proto_prompt))
 
-#     # for i in range(100):
-#     #     latest_iteration.text(f'Iteration {i+1}')
-#     #     bar.progress(i + 1)
-#     #     time.sleep(.5)
+    "## Surprise"
 
-#     # generated_results = query_generated_results(suno_api_key, song_id)
+    latest_iteration = st.empty()
+    bar = st.progress(0)
 
-#     # # Clear the placeholders
-#     # latest_iteration.empty()
-#     # bar.empty()
+    for i in range(10):
+        latest_iteration.text(f'Making magic {i+1}')
+        bar.progress(i + 1)
+        time.sleep(.5)
 
-#     # "### ...and now we\'re done! Your very own marketing jingle 🎶"
-#     # st.audio(generated_results[0]["audio_url"])
-#     # st.write(title)
-#     # st.write(lyrics)
+    generated_results = query_generated_results(suno_api_key, song_id)
 
-#     # "## Quota"
-#     # quota_info = check_remaining_quota(suno_api_key)
-#     # st.write(quota_info)
+    # Clear the placeholders
+    latest_iteration.empty()
+    bar.empty()
+
+    "### ...and now we\'re done! Your very own marketing jingle 🎶"
+    st.audio(generated_results[0]["audio_url"])
+    st.write(title)
+    st.write(lyrics)
+
+    "## Quota"
+    quota_info = check_remaining_quota(suno_api_key)
+    st.write(quota_info)
