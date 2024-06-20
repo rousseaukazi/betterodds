3ad4477e39c383fa2406dcd2be4c4d17be4e1c5e
rousseaukazi <kazi.rousseau@gmail.com>
2024-06-20 08:44:17 -0400

    Update app.py

--- app.py
+++ app.py
@@ -78,7 +78,7 @@ def OneLiners():
         text_value = oneliner.text_area("Idea", st.session_state['idea'])
         if oneliner.text_area:
             oneliner.empty()
-            st.text_area("Idea",text_value)
+            text_value
     else:
         "There's no idea."
 
