82fcf748d4c9f9803ff186e8e4a624b3270c2d15
rousseaukazi <kazi.rousseau@gmail.com>
2024-06-20 10:18:17 -0400

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def Home():
     if idea:
         st.session_state['idea'] = idea
     if 'idea' in st.session_state:
-        st.write(st.session_state['idea'])
+        st.write("**Current Idea:** " + st.session_state['idea'])
 
 def OneLiners():
     st.title("One Liners")
