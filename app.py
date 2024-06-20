a3d42e94c4f770a80fcd32bcda6bf9b699a73caa
rousseaukazi <kazi.rousseau@gmail.com>
2024-06-20 10:03:31 -0400

    Update app.py

--- app.py
+++ app.py
@@ -89,13 +89,14 @@ def OneLiners():
 
 def Domains():
     st.title("Domains")
+    domain = st.empty()
     if 'idea' in st.session_state and 'domain_prompt' not in st.session_state:
-        domain_prompt_default = st.text_area("Prompt", "I'm starting a company. This is my idea " + st.session_state['idea'] + ". Please provide me with 3 different domains I can use in my seed deck. Just provide the domain name, a reason why, and nothing else.", key="domain")
+        domain_prompt_default = domain.text_area("Prompt", "I'm starting a company. This is my idea " + st.session_state['idea'] + ". Please provide me with 3 different domains I can use in my seed deck. Just provide the domain name, a reason why, and nothing else.", key="domain")
         ChatGPT(domain_prompt_default)
         if st.button("submit",type="primary") or domain_prompt_default:
             st.session_state['domain_prompt'] = domain_prompt_default
     elif 'idea' in st.session_state and 'domain_prompt' in st.session_state:
-        domain_prompt_edit = st.text_area("Prompt", st.session_state['domain_prompt'])
+        domain_prompt_edit = domain.text_area("Prompt", st.session_state['domain_prompt'])
         ChatGPT(domain_prompt_edit)
         if st.button("submit",type="primary") or domain_prompt_edit:
             st.session_state['ol_prompt'] = domain_prompt_edit
