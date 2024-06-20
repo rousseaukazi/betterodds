de10913952913a386d05118087e2974274538bd0
rousseaukazi <kazi.rousseau@gmail.com>
2024-06-20 11:11:30 -0400

    Update app.py

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ def generate_music(api_key, title, prompt, mv="chirp-v3-5", continue_at=None, co
     
     data = {
         "title": title,
-        "tags": 'pop, upbeat',
+        "tags": 'edm, techno, upbeat',
         "prompt": prompt,
         "mv": mv
     }
@@ -220,38 +220,6 @@ def Jingle():
             st.write(st.session_state['jingle_lyrics'])
     else:
         st.write("Please enter an idea on the Input page.")
-
-# def Jingle():
-#         song_prompt = "Create 1 stanzas of song lyrics for a pop, marketing song about my business idea. It should be a short jingle. Here's the idea: " + st.session_state['idea'] + ". Just provide the lyrics no extranerous or confirmation text. It should start with (Verse 1)."
-#         song_title_prompt = "Create a title for a poppy, marketing song for my business idea. Here's the idea: " + st.session_state['idea'] + ". Just provide the title, nothing else."
-
-#         title = ChatGPT(song_title_prompt)
-#         lyrics = ChatGPT(song_prompt)
-
-#         music_generation_info = generate_music(suno_api_key, title, lyrics)
-#         song_id = music_generation_info[0]["song_id"]
-#         song_id = [song_id]
-
-#         "## Surprise"
-#         latest_iteration = st.empty()
-#         bar = st.progress(0)
-
-#         for i in range(100):
-#             latest_iteration.text(f'Making magic {i+1}')
-#             bar.progress(i + 1)
-#             time.sleep(.5)
-
-#         generated_results = query_generated_results(suno_api_key, song_id)
-
-#         # Clear the placeholders
-#         latest_iteration.empty()
-#         bar.empty()
-
-#         "### ...and now we\'re done! Your very own marketing jingle 🎶"
-#         generated_results = query_generated_results(suno_api_key, song_id)
-#         st.audio(generated_results[0]["audio_url"])
-#         st.write(title)
-#         st.write(lyrics)
 
 # NAVIGATION
 pages = {
