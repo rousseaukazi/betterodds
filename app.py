372975ca0d5415c41b1b0bcb929ff0b02534cd2a
rousseaukazi <kazi.rousseau@gmail.com>
2024-06-21 13:12:35 -0400

    Update app.py

--- app.py
+++ app.py
@@ -345,7 +345,9 @@ def Video():
         response = requests.post(url, json=payload, headers=headers)
 
         json_data = response.json()
+        video_id = json_data["id"]
         st.write(json_data)
+        st.write(video_id)
 
     def getVideo():
         import requests
