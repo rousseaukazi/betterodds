e20b7f1725bb6d62a846f998ddc27e1024fa2217
rousseaukazi <kazi.rousseau@gmail.com>
2024-06-22 10:51:37 -0400

    Update app.py

--- app.py
+++ app.py
@@ -140,7 +140,7 @@ def remove_bg(image_url):
     )
     if response.status_code == requests.codes.ok:
         with open('no-bg.png', 'wb') as out:
-            out.write(response.content)
+            return out.write(response.content)
     else:
         print("Error:", response.status_code, response.text)
 
